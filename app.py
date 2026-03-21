a80e231bb0b2af29c98fbe2b0cb922e0a69c962f
mildredbr-design <mildredbr@gmail.com>
2026-03-21 17:57:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def calcular_tae(cuotas, fechas, capital, tin, duracion):
     # Condicional por capital < 6000
     if capital < 6000:
         r = Decimal(str(tin)) / Decimal("100") / Decimal("12")
-        tae = ((1 + r/12)) ** 12 - 1) * 100
+        tae = (((1 + r/12) ** (12))- 1) * 100
         return round(float(tae), 2)
 
     tiempos = [0.0]
